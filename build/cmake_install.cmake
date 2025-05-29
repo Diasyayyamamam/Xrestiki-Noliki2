ccadc3059941f221458b5b7920c7c99c4e32765b
Diana <DruzhenkoDiana@gmail.com>
2025-05-29 23:34:48 +0300

    Добавлен файл автоматической сборки

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/OPI8")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/OPI8")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
